bd9cfb1816710755eadab4160cd8e2e24e4d6526
Fernando Toscano <fernandotoscanofilho@gmail.com>
2026-07-14 23:58:22 -0300

    criação das componentizações e finalizado a componetização Cabeca

--- src/meuApp.js
+++ src/meuApp.js
@@ -2,6 +2,46 @@ import { Text, View, TextInput, Image, StyleSheet} from "react-native";
 import { useState } from "react";
 
 export default function Index() {
+return (
+
+<View style={estilo.container}> 
+
+    <View style={{ 
+        backgroundColor: '#0A8F11',
+        flex: 1,
+    }}>
+        <Cabeca />
+        
+    </View>
+
+    <View style={{
+      flex: 4,
+      backgroundColor: "white"
+    }}>
+
+    </View>
+
+</View>
+
+)}
+
+export function Cabeca() {
+return (
+<View style={estilo.orgCorpo}>
+
+    <Text style={estilo.titulo}>
+    BEM VINDOS
+    </Text>
+
+    <Text style={estilo.subtitulo}>
+    Siga abaixo as instruções:
+    </Text>
+
+</View>
+
+)}
+
+export function Meio() {
 
     const [nome,  setNome] = useState('');
     const [senha, setSenha] = useState('');
@@ -18,8 +58,17 @@ export default function Index() {
     )
 }
 
+export function Final() {
+
+}
+
 const estilo = StyleSheet.create ({
-    container: { 
+
+    container: {
+        flex: 1
+    },
+
+    orgCorpo: { 
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
@@ -28,11 +77,14 @@ const estilo = StyleSheet.create ({
     titulo: {
         fontSize: 20,
         textAlign: 'center',
+        color: 'white'
     },
 
     subtitulo: {
         fontSize: 15,
-        textAlign: 'center' 
+        textAlign: 'center',
+        color: 'white',
+        marginTop: 10
     },
 
     input: {
@@ -44,6 +96,5 @@ const estilo = StyleSheet.create ({
         width: "80%"
     
     }
-
 
 })
